bc9fb7aa177f041f6edbadadf80625b920ec24e7
Israel Abraham <israelvictory87@gmail.com>
2022-04-11 08:31:43 +0100

    Update authorization_headers

--- async_paystack/config.py
+++ async_paystack/config.py
@@ -5,8 +5,16 @@ from typing import Dict
 # Paystack Secret Key
 PAYSTACK_SECRET_KEY = environ.get('PAYSTACK_SECRET_KEY')
 
-# Authorization Headers
+# Authorization Headers Definition
 def authorization_headers(secret_key:str) -> Dict[str,str]:
+    """
+    It takes a secret key and returns a dictionary of headers that can be 
+    used to make requests to the API
+    
+    :param secret_key: The secret key you got from the API page
+    :type secret_key: str
+    :return: A dictionary with the key "Authorization" and the value "Bearer {secret_key}"
+    """
     headers = {
         "Authorization": f"Bearer {secret_key}",
         "Content-Type": "application/json"
