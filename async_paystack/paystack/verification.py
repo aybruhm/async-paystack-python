47fb7993e6530335672829c54d237a8709a73bc1
Abram <israelvictory87@gmail.com>
2022-09-17 12:50:39 +0100

    Inherited from base Paystack class and created verification api methods

---
+++ async_paystack/paystack/verification.py
@@ -0,0 +1,110 @@
+# Own Imports
+from async_paystack.services import PayStack
+
+# Typing Imports
+from typing import Dict, Tuple
+
+# Third Party Imports
+import httpx
+
+# Native Imports
+import json
+
+
+class Verification(PayStack):
+    """The Verification API allows you perform KYC processes"""
+    
+    async def resolves_account_number(self, account_number:str, bank_code:str) -> Tuple[Dict, Dict]:
+        """
+        This function verifies the account number
+        
+        :param account_number: The account number of the bank account you want to verify
+        :type account_number: str
+        :param bank_code: The bank code of the bank you want to verify the account number for
+        :type bank_code: str
+        :return: A tuple of two dictionaries.
+        """
+        
+        async with httpx.AsyncClient() as client:
+            
+            url = self.BASE_URL + f"bank/resolve?account_number={account_number}&bank_code={bank_code}"
+            response = await client.get(url, headers=json.dumps(self.headers))
+            
+            if response.status_code == 200:
+                response_data = response.json()
+                return response_data["status"], response_data["data"]
+        
+            else:
+                response_data = response.json()
+                return response_data["status"], response_data["message"]
+            
+            
+    async def validate_account_number(
+        self, bank_code:str, country_code:str, account_number:str, 
+        account_name:str, account_type:str, document_type:str, document_number:str) -> Tuple[Dict, Dict]:
+        """
+        This function validates a customer's bank account number
+        
+        :param bank_code: The bank code of the customer's bank
+        :type bank_code: str
+        :param country_code: The two digit ISO code of the customer's bank
+        :type country_code: str
+        :param account_number: Customer's account number
+        :type account_number: str
+        :param account_name: Customer's first and last name registered with their bank
+        :type account_name: str
+        :param account_type: This can take one of: [ personal, business ]
+        :type account_type: str
+        :param document_type: This can take one of: [ identityNumber, passportNumber,
+        businessRegistrationNumber ]
+        :type document_type: str
+        :param document_number: This is the customer's identity number
+        :type document_number: str
+        :return: A tuple of two dictionaries.
+        """
+        
+        async with httpx.AsyncClient() as client:
+            
+            data = {
+                "bank_code": f"{bank_code}",
+                "country_code": f"{country_code}",
+                "account_number": f"{account_number}",
+                "account_name": f"{account_name}",
+                "account_type": f"{account_type}",
+                "document_type": f"{document_type}",
+                "document_number": f"{document_number}"
+            }
+            url = self.BASE_URL + "bank/validate"
+            response = await client.post(url, headers=json.dumps(self.headers), data=json.dumps(data))
+            
+            if response.status_code == 200:
+                response_data = response.json()
+                return response_data["status"], response_data["data"]
+            
+            else:
+                response_data = response.json()
+                return response_data["status"], response_data["message"]
+            
+            
+    async def resolve_card_bin(self, bin:str) -> Tuple[Dict, Dict]:
+        """
+        This function verifies a card bin and get more information about the customer's
+
+        
+        :param bin: The first 6 digits of the card number
+        :type bin: str
+        :return: A tuple of two dictionaries.
+        """
+        
+        async with httpx.AsyncClient() as client:
+            
+            url = self.BASE_URL + f"decision/bin/{bin}"
+            response = await client.get(url=url, headers=json.dumps(self.headers))
+            
+            if response.status_code == 200:
+                response_data = response.json()
+                return response_data["status"], response_data["data"]
+            
+            else:
+                response_data = response.json()
+                return response_data["status"], response_data["message"]
