9468960f649042d8ff23409f7a1c2b853a1869ae
Abram <israelvictory87@gmail.com>
2022-09-18 18:35:55 +0100

    Removed validate_accoun_number api method

--- async_paystack/paystack/transfers.py
+++ async_paystack/paystack/transfers.py
@@ -15,56 +15,7 @@ class Transfers(PayStack):
     """
     The Transfer API Wrapper allows you to send money to bank accounts and mobile money wallet.
     """
-            
-    async def validate_account_number(
-        self, bank_code:str, country_code:str, account_number:str, 
-        account_name:str, account_type:str, document_type:str, document_number:str) -> Tuple[Dict, Dict]:
-        """
-        This function validates a customer's bank account number
         
-        :param bank_code: The bank code of the customer's bank
-        :type bank_code: str
-        :param country_code: The two digit ISO code of the customer's bank
-        :type country_code: str
-        :param account_number: Customer's account number
-        :type account_number: str
-        :param account_name: Customer's first and last name registered with their bank
-        :type account_name: str
-        :param account_type: This can take one of: [ personal, business ]
-        :type account_type: str
-        :param document_type: This can take one of: [ identityNumber, passportNumber,
-        businessRegistrationNumber ]
-        :type document_type: str
-        :param document_number: This is the customer's identity number
-        :type document_number: str
-        :return: A tuple of two dictionaries.
-
-        See More: https://paystack.com/docs/api/#verification-validate-account
-        """
-        
-        async with httpx.AsyncClient() as client:
-            
-            data = {
-                "bank_code": f"{bank_code}",
-                "country_code": f"{country_code}",
-                "account_number": f"{account_number}",
-                "account_name": f"{account_name}",
-                "account_type": f"{account_type}",
-                "document_type": f"{document_type}",
-                "document_number": f"{document_number}"
-            }
-            url = self.BASE_URL + "bank/validate"
-            response = await client.post(url, headers=json.dumps(self.headers), data=json.dumps(data))
-            
-            if response.status_code == 200:
-                response_data = response.json()
-                return response_data["status"], response_data["data"]
-            
-            else:
-                response_data = response.json()
-                return response_data["status"], response_data["message"]
-    
-    
     async def create_transfer_recipient(self, nuban:str, name:str, account_number:str, bank_code:str, currency:str) -> Tuple[Dict, Dict]:
         """
         This function creates a transfer recipient
