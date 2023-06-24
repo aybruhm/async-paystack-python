f5649a0d2c80e125c72fb00f5bb1a4c08217df62
Abram <israelvictory87@gmail.com>
2023-06-24 16:40:01 +0100

    Refactor: modified base async api wrapper for paystack

--- async_paystack/services.py
+++ async_paystack/services.py
@@ -1,11 +1,9 @@
-# Native Imports
-import sys, os
-
-# Own Imports
-from async_paystack import config
+# Stdlib Imports
+import os
+import sys
 
 # Third party Imports
-from decouple import config as env_config
+from decouple import config as env
 
 
 
@@ -15,19 +13,17 @@ dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(dir))
 
 
-
 class PayStack:
     """
     Base Paystack Async API Wrapper
-    
-    `SECRET_KEY:` do not expose this in production
-    `BASE_URL: ` base url of paystack api
     """
-    # global config
-    # Secret_Key: Do not expose this in production
-    PAYSTACK_SECRET_KEY = config.paystack_secret_key(PAYSTACK_SECRET_KEY = env_config("PAYSTACK_SECRET_KEY"))
-    BASE_URL = "https://api.paystack.co/"
     
-    # authentication headers for paystack
-    headers = config.authorization_headers(secret_key=PAYSTACK_SECRET_KEY)
+    def __init__(self) -> None:
+        self.base_url = env("PAYSTACK_BASE_URL")
+        self.secret_key = env("PAYSTACK_SECRET_KEY")
     
+    def headers(self) -> dict:
+        return {
+        "Authorization": f"Bearer {self.secret_key}",
+        "Content-Type": "application/json"
+    }
